41272dea780a3acb6d4001b948b0ba3c0841b6c2
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-08 10:43:26 +0700

    Make Tutorial Recipe

--- app/Filament/Resources/RecipeResource/RelationManagers/TutorialsRelationManager.php
+++ app/Filament/Resources/RecipeResource/RelationManagers/TutorialsRelationManager.php
@@ -28,7 +28,12 @@ class TutorialsRelationManager extends RelationManager
     {
         return $table
             ->recordTitleAttribute('name')
-            ->columns([
+            ->columns([Tables\Columns\TextColumn::make('Squence')
+                ->label('No')
+                ->getStateUsing(function ($rowLoop, $record) {
+                    return $rowLoop->iteration;
+                }),
+
                 Tables\Columns\TextColumn::make('name'),
             ])
             ->filters([
