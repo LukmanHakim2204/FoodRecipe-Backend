21c597432a7b1fc05e0c3b3daa5155a39578d2b0
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-08 13:57:38 +0700

    Create Detail Api Recipe

---
+++ app/Http/Resources/Api/RecipeResource.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use App\Http\Resources\Api\CategoryResource;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RecipeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'slug' => $this->slug,
+            'url_file' => $this->url_file,
+            'url_video'=>$this->url_video,
+            'thumbnail' => $this->thumbnail,
+            'about' => $this->about,
+            'category'=> new CategoryResource($this->whenLoaded('category')),
+            'recipe_ingredients'=> new RecipeIngredientResource($this->whenLoaded('recipe_ingredients')),
+            'photos' =>  RecipePhotoResource::collection($this->whenLoaded('photos')),
+            'tutorials' => RecipeTutorialResource::collection($this->whenLoaded('tutorials')),
+            'author' =>  new RecipeAuthorResource ($this->whenLoaded('author')),
+        ];
+    }
+}
