3b4cc8437d6a91ceb76515c9c5def6c2251edaf6
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-08 14:15:03 +0700

    Create Search Recipe

---
+++ app/Http/Controllers/Api/SearchController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Recipe;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Api\RecipeResource;
+
+class SearchController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = $request->input('query');
+
+        $recipes = Recipe::where('name', 'LIKE', "%{$query}%")->get();
+        return RecipeResource::collection($recipes);
+    }
+}
