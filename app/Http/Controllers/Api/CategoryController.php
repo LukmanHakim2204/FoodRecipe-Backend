3c41a4b58ca2462991a2a5a580885408bf469571
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-08 12:37:12 +0700

    installasi Api and fix api-category

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -11,13 +11,14 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        $categories = Category::whitCount('recipes')->get();
+        $categories = Category::withCount('recipes')->get();
         return CategoryResource::collection($categories);
     }
 
     public function show(Category $category)
     {
         $category->load('recipes');
+        $category->loadCount('recipes');
         return new CategoryResource($category);
     }
 }
