14289581fff6ffe6dfb274c209ecdd60d112a093
Lukman <103345757+LukmanHakim2204@users.noreply.github.com>
2025-01-24 22:36:27 +0700

    create an api-key to protect the API

--- routes/api.php
+++ routes/api.php
@@ -9,14 +9,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-
-Route::get('/recipes/search', [SearchController::class, 'index']);
-
-Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
-Route::apiResource('/categories', CategoryController::class);
-
-
-
-Route::get('/recipe/{recipe:slug}', [RecipeController::class, 'show']);
-Route::apiResource('/recipes', RecipeController::class);
+Route::middleware('api_key')->group(function () {
+    //search
+    Route::get('/recipes/search', [SearchController::class, 'index']);
+    //category
+    Route::get('/category/{category:slug}', [CategoryController::class, 'show']);
+    Route::apiResource('/categories', CategoryController::class);
+    //recipe
+    Route::get('/recipe/{recipe:slug}', [RecipeController::class, 'show']);
+    Route::apiResource('/recipes', RecipeController::class);
+});
